2c4e48c86e9fece46750bd7d0eb4b9b6ebf0841f
BENJAMIN\avalo <avalos.benjamin2003@gmail.com>
2024-07-17 20:33:18 -0700

    Starting the dealership class

--- Dealership.py
+++ Dealership.py
@@ -1,1 +1,18 @@
 print('hi')
+
+class Dealership:
+
+    def __init__(self):
+        self.staff = []
+        self.inventory = []
+        self.revenue = 0
+
+    def get_revenue(self):
+        return self.revenue
+    
+    def sell_car(self, model_name, used, employee_who_sold):
+
+        self.staff.remove(model_name)
+        for staff in self.staff:
+            if employee_who_sold == staff:
+                employee_who_sold += 1
