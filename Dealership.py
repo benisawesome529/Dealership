c0f7ddd14a776e3f79691eb536bc43a822b67395
BENJAMIN\avalo <avalos.benjamin2003@gmail.com>
2024-07-17 21:00:16 -0700

    added more functions idk if theyre right

--- Dealership.py
+++ Dealership.py
@@ -1,4 +1,3 @@
-print('hi')
 
 class Dealership:
 
@@ -6,6 +5,7 @@ class Dealership:
         self.staff = []
         self.inventory = []
         self.revenue = 0
+        self.car_models = [ "1", "2", "3", "4"]
 
     def get_revenue(self):
         return self.revenue
@@ -16,3 +16,17 @@ class Dealership:
         for staff in self.staff:
             if employee_who_sold == staff:
                 employee_who_sold += 1
+
+    def add_car(self, model_name, used):
+        if model_name in self.car_models:
+            self.inventory.append(model_name)
+    
+    def get_cars(self):
+        return self.inventory
+    
+    def add_employee(self, name, position):
+        new_employee = Employee(name,position)
+        self.staff.append(new_employee)
+
+    def fire(self, name):
+        self.staff.remove(name)
